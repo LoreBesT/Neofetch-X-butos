41461676debe764c9d6ff82ae9ff5cad34872bda
Lorenzo <102426474+LoreBesT@users.noreply.github.com>
2024-08-01 23:59:45 +0200

    Add others cpu informations

--- main.c
+++ main.c
@@ -9,41 +9,93 @@
 #define RESET "\e[0m"
 
 // Funzione per ottenere le informazioni sulla CPU tramite CPUID
-void get_cpu_info(char *cpu_info) {
+void get_processor_version(char *vendor, char *brand) {
     unsigned int eax, ebx, ecx, edx;
+    unsigned int vendor_id[3];
 
+    // Ottieni l'ID del fornitore del processore
+    eax = 0x00000000; // Funzione 0
     __asm__ (
-        "movl $0, %%eax\n\t"      // Imposta EAX a 0 per ottenere il Vendor ID
-        "cpuid\n\t"                // Chiama CPUID
-        "movl %%ebx, %1\n\t"      // Salva EBX
-        "movl %%edx, %2\n\t"      // Salva EDX
-        "movl %%ecx, %3\n\t"      // Salva ECX
-        : "=a"(eax), "=r"(ebx), "=r"(edx), "=r"(ecx) // Output
-        :                           // Nessun input
-        : "ebx", "edx", "ecx"      // Registri modificati
+        "cpuid"
+        : "=b" (vendor_id[0]), "=d" (vendor_id[1]), "=c" (vendor_id[2])
+        : "a" (eax)
     );
 
-    // Formattiamo il Vendor ID come una stringa
-    sprintf(cpu_info, "%c%c%c%c%c%c%c%c%c%c%c%c",
-            (ebx & 0xFF), (ebx >> 8 & 0xFF), (ebx >> 16 & 0xFF), (ebx >> 24 & 0xFF),
-            (edx & 0xFF), (edx >> 8 & 0xFF), (edx >> 16 & 0xFF), (edx >> 24 & 0xFF),
-            (ecx & 0xFF), (ecx >> 8 & 0xFF), (ecx >> 16 & 0xFF), (ecx >> 24 & 0xFF));
+    // Copia l'ID del fornitore del processore
+    snprintf(vendor, 13, "%c%c%c%c%c%c%c%c%c%c%c%c",
+             (char)(vendor_id[0] & 0xFF), (char)((vendor_id[0] >> 8) & 0xFF),
+             (char)((vendor_id[0] >> 16) & 0xFF), (char)((vendor_id[0] >> 24) & 0xFF),
+             (char)(vendor_id[1] & 0xFF), (char)((vendor_id[1] >> 8) & 0xFF),
+             (char)((vendor_id[1] >> 16) & 0xFF), (char)((vendor_id[1] >> 24) & 0xFF),
+             (char)(vendor_id[2] & 0xFF), (char)((vendor_id[2] >> 8) & 0xFF),
+             (char)((vendor_id[2] >> 16) & 0xFF), (char)((vendor_id[2] >> 24) & 0xFF));
+    vendor[12] = '\0'; // Aggiungi terminatore di stringa
+
+    // Ottieni il nome del processore
+    eax = 0x80000002; // Funzione 0x80000002
+    __asm__ (
+        "cpuid"
+        : "=a" (eax), "=b" (ebx), "=c" (ecx), "=d" (edx)
+        : "a" (eax)
+    );
+    snprintf(brand, 17, "%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c",
+             (char)(eax & 0xFF), (char)((eax >> 8) & 0xFF),
+             (char)((eax >> 16) & 0xFF), (char)((eax >> 24) & 0xFF),
+             (char)(ebx & 0xFF), (char)((ebx >> 8) & 0xFF),
+             (char)((ebx >> 16) & 0xFF), (char)((ebx >> 24) & 0xFF),
+             (char)(ecx & 0xFF), (char)((ecx >> 8) & 0xFF),
+             (char)((ecx >> 16) & 0xFF), (char)((ecx >> 24) & 0xFF),
+             (char)(edx & 0xFF), (char)((edx >> 8) & 0xFF),
+             (char)((edx >> 16) & 0xFF), (char)((edx >> 24) & 0xFF));
+
+    eax = 0x80000003; // Funzione 0x80000003
+    __asm__ (
+        "cpuid"
+        : "=a" (eax), "=b" (ebx), "=c" (ecx), "=d" (edx)
+        : "a" (eax)
+    );
+    snprintf(brand + 16, 17, "%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c",
+             (char)(eax & 0xFF), (char)((eax >> 8) & 0xFF),
+             (char)((eax >> 16) & 0xFF), (char)((eax >> 24) & 0xFF),
+             (char)(ebx & 0xFF), (char)((ebx >> 8) & 0xFF),
+             (char)((ebx >> 16) & 0xFF), (char)((ebx >> 24) & 0xFF),
+             (char)(ecx & 0xFF), (char)((ecx >> 8) & 0xFF),
+             (char)((ecx >> 16) & 0xFF), (char)((ecx >> 24) & 0xFF),
+             (char)(edx & 0xFF), (char)((edx >> 8) & 0xFF),
+             (char)((edx >> 16) & 0xFF), (char)((edx >> 24) & 0xFF));
+
+    eax = 0x80000004; // Funzione 0x80000004
+    __asm__ (
+        "cpuid"
+        : "=a" (eax), "=b" (ebx), "=c" (ecx), "=d" (edx)
+        : "a" (eax)
+    );
+    snprintf(brand + 32, 17, "%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c",
+             (char)(eax & 0xFF), (char)((eax >> 8) & 0xFF),
+             (char)((eax >> 16) & 0xFF), (char)((eax >> 24) & 0xFF),
+             (char)(ebx & 0xFF), (char)((ebx >> 8) & 0xFF),
+             (char)((ebx >> 16) & 0xFF), (char)((ebx >> 24) & 0xFF),
+             (char)(ecx & 0xFF), (char)((ecx >> 8) & 0xFF),
+             (char)((ecx >> 16) & 0xFF), (char)((ecx >> 24) & 0xFF),
+             (char)(edx & 0xFF), (char)((edx >> 8) & 0xFF),
+             (char)((edx >> 16) & 0xFF), (char)((edx >> 24) & 0xFF));
 }
 
 int main() {
     char os_name[] = "Butos";  // Nome del sistema operativo
     float os_version = 0.0;
     char cpu_info[100];      // Buffer per le informazioni sulla CPU
+    char vendor[13];
+    char brand[49]; // 3 * 16 + 1 per il terminatore
 
-    // Otteniamo le informazioni sulla CPU
-    get_cpu_info(cpu_info);
+    get_processor_version(vendor, brand);
 
-    // Stampa le informazioni
     printf(RED"OS: "RESET);
     printf("%s ", os_name);
     printf("%.1f\n",os_version);
     printf(RED"CPU: "RESET);
-    printf("%s\n", cpu_info);
+    printf("%s ", brand);
+    printf("%s\n", vendor);
 
     return 0;
 }
